7f5a092b36d404a6d290a6f4259a3fb1e61d100d
TAN.TON <nhattan2007@gmail.com>
2021-04-26 12:02:21 +0700

    Commit before pull dev branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
 	{
-		path: "",
-		
+		path: "hanh-chinh",
+		component: ,
+		children: [
+			{
+				path: "",
+				loadChildren: () => import("./general-room/general-room.module").then(g => g.GeneralRoomModule)
+			}
+		]
+	},
+	{
+		path:"error",
+		loadChildren: () => import("./errors/errors.module").then(e => e.ErrorsModule)
+	},
+	{
+		path: '**',
+		component: NotFoundComponent
 	}
 ];
 
