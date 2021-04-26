b72d49fa9009aaae647d437857286311a65e7ea2
TAN.TON <nhattan2007@gmail.com>
2021-04-26 13:50:37 +0700

    Commit to check error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { GeneralRoomLayoutComponent } from './_shared/components/general-room-layout/general-room-layout.component';
 
 const routes: Routes = [
 	{
-		path: "hanh-chinh",
-		component: ,
+		path: "",
+		component: GeneralRoomLayoutComponent,
 		children: [
 			{
 				path: "",
