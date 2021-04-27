06dadbe595058f6d41837a7246509b33a204931d
phuc212 <leconghoangphucgmail.com>
2021-04-27 15:43:50 +0700

    update component ognization

---
+++ src/app/general-room/ogranization-list/ogranization-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ogranization-list',
+  templateUrl: './ogranization-list.component.html',
+  styleUrls: ['./ogranization-list.component.scss']
+})
+export class OgranizationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
