e1b5a518d8d2b63133881d55e7a58eeaad5763d8
TAN.TON <nhattan2007@gmail.com>
2021-05-09 22:24:48 +0700

    Commit to pull dev branch 09.05.2021

--- src/app/_shared/interceptor/tokenInterceptor.ts
+++ src/app/_shared/interceptor/tokenInterceptor.ts
@@ -8,16 +8,10 @@ import { Observable } from 'rxjs';
 /** Inject With Credentials into the request */
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-	private headers = new HttpHeaders({
-		'Content-Type':'application/json; charset=utf-8;' 
-		,'Accept':'*/*'
-	  });
 
   	intercept(req: HttpRequest<any>, next: HttpHandler):
     			Observable<HttpEvent<any>> {
-        
 	req = req.clone({
-		headers: this.headers,
 		withCredentials: true
 	});
       
