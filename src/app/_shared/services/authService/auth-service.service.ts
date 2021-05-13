7c5d044151e02929314941cfda77fae4b09e9dfa
TAN.TON <nhattan2007@gmail.com>
2021-05-13 16:15:18 +0700

    Update auth service

--- src/app/_shared/services/authService/auth-service.service.ts
+++ src/app/_shared/services/authService/auth-service.service.ts
@@ -5,6 +5,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AccountLogin, AccountLoginRes } from '../../models/accountLogin.Model';
+import { DepartmentId } from '../../models/department.Models';
 import { NotificationStatus } from '../../models/system.Models';
 import { UserInfoRes } from '../../models/user.Models';
 import { ConfigService } from '../config/config.service';
@@ -29,7 +30,7 @@ export class AuthService {
 			.subscribe(
 				(data: AccountLoginRes) => {
 					this.notificationService.emitNotification({message: "Đăng nhập thành công", status: NotificationStatus.Success})
-					this.router.navigateByUrl("/auth/phong-tong-hop/tao-thong-tin-benh-nhan")
+					this.getUserInfo();
 				},
 				(error) => {
 					if(error.status == 401){
@@ -49,9 +50,51 @@ export class AuthService {
 			})
 	}
 
-	getUserInfo(): Observable<UserInfoRes>{
-		return this.httpClient.get(`${this.apiDomain}/userInfo`)
-			.pipe(map(res => res as UserInfoRes))
+	getUserInfo(){
+		this.httpClient.get(`${this.apiDomain}/userInfo`)
+			.pipe(map(res => res as UserInfoRes)).subscribe((res) => {
+				this.userInfo = res as UserInfoRes
+				console.log(res)
+				switch (res.departmentId) {
+					case DepartmentId.da_lieu:
+						this.router.navigateByUrl('auth/phong-kham/da-lieu')
+						break;
+					case DepartmentId.mat:
+						this.router.navigateByUrl('auth/phong-kham/mat')
+						break;
+					case DepartmentId.than_kinh:
+						this.router.navigateByUrl('auth/phong-kham/than-kinh')
+						break;
+					case DepartmentId.ngoai_khoa:
+						this.router.navigateByUrl('auth/phong-kham/ngoai-khoa')
+						break;
+					case DepartmentId.chan_doan_hinh_anh:
+						this.router.navigateByUrl('auth/phong-kham/chan-doan-hinh-anh')
+						break;
+					case DepartmentId.noi_khoa:
+						this.router.navigateByUrl('auth/phong-kham/noi-khoa')
+						break;
+					case DepartmentId.tai_mui_hong:
+						this.router.navigateByUrl('auth/phong-kham/tai-mui-hong')
+						break;
+					case DepartmentId.xet_nghiem:
+						this.router.navigateByUrl('auth/phong-kham/xet-nghiem')
+						break;
+					case DepartmentId.phu_san:
+						this.router.navigateByUrl('auth/phong-kham/phu-san')
+						break;
+					case DepartmentId.rang_ham_mat:
+						this.router.navigateByUrl('auth/phong-kham/rang-ham-mat')
+						break;
+
+					default:
+						break;
+				}
+			},(err) => {
+				if(err.status == 401){
+					this.router.navigate([""]);
+				}
+			})
 	}
 
 	setNavigateList(){
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,49 +30,6 @@ export class AppComponent implements OnInit {
 			}, 3 * 1000);
 		})
 
-		this.authService.getUserInfo()
-			.subscribe((res) => {
-				this.authService.userInfo = res as UserInfoRes
-				console.log(res)
-				switch (res.departmentId) {
-					case DepartmentId.da_lieu:
-						this.router.navigateByUrl('auth/phong-kham/da-lieu')
-						break;
-					case DepartmentId.mat:
-						this.router.navigateByUrl('auth/phong-kham/mat')
-						break;
-					case DepartmentId.than_kinh:
-						this.router.navigateByUrl('auth/phong-kham/than-kinh')
-						break;
-					case DepartmentId.ngoai_khoa:
-						this.router.navigateByUrl('auth/phong-kham/ngoai-khoa')
-						break;
-					case DepartmentId.chan_doan_hinh_anh:
-						this.router.navigateByUrl('auth/phong-kham/chan-doan-hinh-anh')
-						break;
-					case DepartmentId.noi_khoa:
-						this.router.navigateByUrl('auth/phong-kham/noi-khoa')
-						break;
-					case DepartmentId.tai_mui_hong:
-						this.router.navigateByUrl('auth/phong-kham/tai-mui-hong')
-						break;
-					case DepartmentId.xet_nghiem:
-						this.router.navigateByUrl('auth/phong-kham/xet-nghiem')
-						break;
-					case DepartmentId.phu_san:
-						this.router.navigateByUrl('auth/phong-kham/phu-san')
-						break;
-					case DepartmentId.rang_ham_mat:
-						this.router.navigateByUrl('auth/phong-kham/rang-ham-mat')
-						break;
-
-					default:
-						break;
-				}
-			},(err) => {
-				if(err.status == 401){
-					this.router.navigate([""]);
-				}
-			})
+		this.authService.getUserInfo();
 	}
 }
